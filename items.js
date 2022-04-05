2da02ef746c187f7ab31d16af21e5154805e6e30
AsmaaFaried <asmaafaried54@gmail.com>
2022-04-05 19:09:09 +0200

    first commit

---
+++ items.js
@@ -0,0 +1,33 @@
+[
+    {
+        "title" : "Burger",
+        "price" : 50,
+        "image" : "Images/burger.jpg",
+        "description":"Burger sandwich"
+    }
+    ,
+    {
+        "title" : "Pasta",
+        "price" : 70,
+        "image" : "Images/pasta1.jpg",
+        "description":"Red souce"
+    }
+    ,
+    {
+        "title" : "Pizza",
+        "price" : 30,
+        "image" : "Images/pizza1.jpg",
+        "description":"with machroom"
+    }
+    ,
+            {
+        "title" : "Pasta",
+        "price" : 120,
+    "image"     : "Images/pasta2.jpg",         
+    "description":"white souce"
+    
+    }
+]
+
+
+
